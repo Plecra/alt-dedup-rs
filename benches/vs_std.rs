5300493c6157b400ad7038d4296ef994b2809c7c
Marli Frost <marli@frost.red>
2020-10-11 03:54:09 +0100

    chore: rustfmt

--- benches/vs_std.rs
+++ benches/vs_std.rs
@@ -1,6 +1,6 @@
 use alt_dedup::NewDedup;
 use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion};
-use rand::{distributions::Standard, Rng, SeedableRng, prelude::SliceRandom};
+use rand::{distributions::Standard, prelude::SliceRandom, Rng, SeedableRng};
 fn compare<T: Clone + core::cmp::PartialEq>(c: &mut Criterion, name: &str, data: Vec<T>) {
     let mut group = c.benchmark_group(name);
     group.bench_function("std", |b| {
@@ -24,17 +24,42 @@ fn compare<T: Clone + core::cmp::PartialEq>(c: &mut Criterion, name: &str, data:
         );
     });
     group.finish();
-
 }
 pub fn criterion_benchmark(c: &mut Criterion) {
     let mut rng = rand::rngs::SmallRng::seed_from_u64(9438591340573909621);
-    compare(c, "Random bytes", (&mut rng).sample_iter(Standard).take(40_000).collect::<Vec<u8>>());
+    compare(
+        c,
+        "Random bytes",
+        (&mut rng)
+            .sample_iter(Standard)
+            .take(40_000)
+            .collect::<Vec<u8>>(),
+    );
     let strings = ["Hello", "wonder", "whistle", "Crunch", "sizzle"];
-    compare(c, "Borrowed strings", (0..8_000).flat_map(|_| strings.choose(&mut rng)).collect());
-    let strings = ["abcdefghijk", "abcde0ghijk", "ab[defghijk", "abcdefg!ijk", ">bcdefghijk"];
-    compare(c, "Borrowed strings of the same size", (0..8_000).flat_map(|_| strings.choose(&mut rng)).collect());
-    compare(c, "Owned strings", (0..1_000).flat_map(|_| strings.choose(&mut rng).map(|s| s.to_string())).collect());
-
+    compare(
+        c,
+        "Borrowed strings",
+        (0..8_000).flat_map(|_| strings.choose(&mut rng)).collect(),
+    );
+    let strings = [
+        "abcdefghijk",
+        "abcde0ghijk",
+        "ab[defghijk",
+        "abcdefg!ijk",
+        ">bcdefghijk",
+    ];
+    compare(
+        c,
+        "Borrowed strings of the same size",
+        (0..8_000).flat_map(|_| strings.choose(&mut rng)).collect(),
+    );
+    compare(
+        c,
+        "Owned strings",
+        (0..1_000)
+            .flat_map(|_| strings.choose(&mut rng).map(|s| s.to_string()))
+            .collect(),
+    );
 }
 
 criterion_group!(benches, criterion_benchmark);
